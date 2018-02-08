e9f3eb391032ca5198025b88dd92185d982b7dfa
Chris Norman <chris.norman2@googlemail.com>
2018-02-08 00:12:12 +0000

    Added emote command.

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,10 +1,10 @@
 """All commands should be imported here."""
 
-from .socials import Say
+from .socials import Say, Emote
 from .misc import Quit, Password, Inventory, Commands, Shutdown, Abort_Shutdown
 from .movement import Go
 
 __all__ = [
     'Say', 'Quit', 'Password', 'Inventory', 'Commands', 'Shutdown',
-    'Abort_Shutdown', 'Go'
+    'Abort_Shutdown', 'Go', 'Emote'
 ]
--- commands/socials.py
+++ commands/socials.py
@@ -1,6 +1,7 @@
 """Social commands: say, emote."""
 
 
+from socials import socials
 from .base import Command
 
 
@@ -15,3 +16,17 @@ class Say(Command):
             character.notify('You say nothing.')
         else:
             character.do_social('%1n say%1s: "{text}"', text=self.rest)
+
+
+class Emote(Command):
+    """Emote something."""
+
+    def on_init(self):
+        self.add_argument('string', nargs='+', help='The string to emote')
+
+    def func(self, character, args):
+        string, perspectives = socials.convert_emote_string(
+            f'%1n|normal {self.rest}', character.match_single, [character]
+        )
+        perspectives.remove(character)
+        character.do_social(string, _others=perspectives)
