16b25baf90ac691f99ec4f7ba7c98ab3e361e1e4
Chris Norman <chris.norman2@googlemail.com>
2018-02-09 23:31:35 +0000

    Added intercepts.

---
+++ intercepts.py
@@ -0,0 +1,26 @@
+"""Provides the Intercept class."""
+
+from attr import attrs, attrib, Factory
+
+
+@attrs
+class Intercept:
+    """Allows text to be gathered from the user, bypassing normal command
+    processing."""
+
+    func = attrib()
+    args = attrib(default=Factory(tuple))
+    kwargs = attrib(default=Factory(dict))
+    multiline = attrib(default=Factory(bool))
+    lines = attrib(default=Factory(list))
+    end = attrib(default=Factory(lambda: '.'))
+    connection = attrib(default=Factory(lambda: None), init=False)
+
+    def feed(self, line):
+        """Add text to self.text."""
+        self.lines.append(line)
+        if not self.multiline or line == self.end:
+            if self.multiline:
+                # We don't want the end character in the buffer.
+                self.lines.pop()
+            self.func('\n'.join(self.lines), *self.args, **self.kwargs)
---
+++ tests/intercept_test.py
@@ -0,0 +1,44 @@
+from pytest import raises
+from intercepts import Intercept
+
+
+class Done(Exception):
+    """Test passes."""
+
+
+def done_single_line(text):
+    assert text == 'testing'
+    raise Done()
+
+
+def done_multi_line(text):
+    assert text == 'hello\nworld'
+    raise Done()
+
+
+def done_args(text, first, second, hello=None):
+    assert text == 'test'
+    assert first == 1
+    assert second == 2
+    assert hello == 'world'
+    raise Done()
+
+
+def test_single_line():
+    i = Intercept(done_single_line)
+    with raises(Done):
+        i.feed('testing')
+
+
+def test_multi_line():
+    i = Intercept(done_multi_line, multiline=True)
+    i.feed('hello')
+    i.feed('world')
+    with raises(Done):
+        i.feed(i.end)
+
+
+def test_args():
+    i = Intercept(done_args, args=(1, 2), kwargs=dict(hello='world'))
+    with raises(Done):
+        i.feed('test')
