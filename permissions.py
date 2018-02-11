83292963bbc76b797bcb3ee454f68e1f7e22884f
Chris Norman <chris.norman2@googlemail.com>
2018-02-11 13:35:39 +0000

    Use functions for check_*.

--- permissions.py
+++ permissions.py
@@ -1,8 +1,7 @@
 """Contains functions for checking character permissions. The functions raise
 MatchError if they are not happy."""
 
-from attr import attrs, attrib
-from db import MatchError
+import db
 
 
 def check_privileges(character, *names):
@@ -10,19 +9,16 @@ def check_privileges(character, *names):
     False raise CommandExit."""
     for name in names:
         if not getattr(character, name):
-            raise MatchError(f'You do not have {name} privileges.')
+            raise db.MatchError(f'You do not have {name} privileges.')
 
 
-@attrs
-class SimpleChecker:
-    """Check privileges without lots of retyping."""
-
-    name = attrib()
-
-    def __call__(self, character):
-        return check_privileges(character, self.name)
+def check_builder(character):
+    return check_privileges(character, 'builder')
 
 
-check_builder = SimpleChecker('builder')
-check_admin = SimpleChecker('admin')
-check_programmer = SimpleChecker('programmer')
+def check_admin(character):
+    return check_privileges(character, 'admin')
+
+
+def check_programmer(character):
+    return check_privileges(character, 'programmer')
